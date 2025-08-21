d9366dff2ba4c807cba225e69aefc213edd0209f
fomenko dmitriy <fomenkodmitriy94@yandex.ru>
2025-08-21 16:56:51 +0300

    Добавлен пагинатор. Без управления

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import BirthdayForm
@@ -25,8 +26,12 @@ def birthday(request, pk=None):
     return render(request, 'birthday/birthday.html', context) 
 
 def birthday_list(request):
+    birthdays = Birthday.objects.order_by('id')
+    paginator = Paginator(birthdays, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     return render(request, 'birthday/birthday_list.html', {
-        'birthdays': Birthday.objects.all()
+        'page_obj': page_obj
     })
 
 def birthday_delete(request, pk):
